75902537800b036aa3660f85a6540112863a6e77
Nico-Gabriel Ruckendorfer <nicogabrielruckendorfer@gmail.com>
2025-04-05 00:49:40 +0200

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fll-robot-game-livestream
+
+The web application for watching the [FIRST® LEGO® League](https://www.first-lego-league.org/) robot game livestream of the regional competition in Hollabrunn, Austria.
---
+++ src/App.js
@@ -0,0 +1,40 @@
+import useResizeObserver from "@react-hook/resize-observer";
+import React, { useRef } from "react";
+import "./App.css";
+
+const App = () => {
+	const topbarRef = useRef(null);
+
+	const updateTeamNameBackgroundCutoutWidth = () => {
+		if (!topbarRef.current) {
+			return;
+		}
+
+		document.documentElement.style.setProperty(
+			"--team-name-background-cutout-width",
+			`${topbarRef.current.clientHeight}px`
+		);
+	};
+
+	useResizeObserver(topbarRef, updateTeamNameBackgroundCutoutWidth);
+
+	return (
+		<div className="app-container">
+			<div ref={topbarRef} className="topbar">
+				<div className="red-team-name-wrapper children-center">
+					<div className="red-team-name bold">Red Team</div>
+				</div>
+				<div className="countdown-timer-wrapper children-center">
+					<div className="countdown-timer">2:30</div>
+				</div>
+				<div className="blue-team-name-wrapper children-center">
+					<div className="blue-team-name bold">Blue Team</div>
+				</div>
+			</div>
+			<div className="red-team-video-wrapper"></div>
+			<div className="blue-team-video-wrapper"></div>
+		</div>
+	);
+};
+
+export default App;
