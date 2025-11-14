1c648863ac035256fe872d2d5d7966b91bb888e7
Nico-Gabriel Ruckendorfer <nicogabrielruckendorfer@gmail.com>
2025-11-14 14:40:12 +0100

    feat(topbar): finish marquee text effect implementation for both sides; only apply if the text is overflowing (this is checked in Topbar.jsx)

--- src/pages/match-stream-page/components/topbar/Topbar.jsx
+++ src/pages/match-stream-page/components/topbar/Topbar.jsx
@@ -1,9 +1,11 @@
 import useResizeObserver from "@react-hook/resize-observer";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import "./Topbar.css";
 
 const Topbar = () => {
 	const topbarRef = useRef(null);
+	const redTeamNameRef = useRef(null);
+	const blueTeamNameRef = useRef(null);
 
 	const updateTeamNameBackgroundCutoutWidth = () => {
 		if (!topbarRef.current) {
@@ -15,29 +17,57 @@ const Topbar = () => {
 			`${topbarRef.current.clientHeight}px`
 		);
 
+		document.documentElement.style.setProperty("--red-team-name-width", `${redTeamNameRef.current.clientWidth}px`);
+
 		document.documentElement.style.setProperty(
-			"--team-name-width",
-			`${document.querySelector(".blue-team-name").clientWidth}px`
+			"--blue-team-name-width",
+			`${blueTeamNameRef.current.clientWidth}px`
 		);
 
 		document.documentElement.style.setProperty(
 			"--team-name-background-width",
-			`${document.querySelector(".blue-team-name-wrapper").clientWidth}px`
+			`${redTeamNameRef.current.parentElement.clientWidth}px`
 		);
+
+		const redTeamName = redTeamNameRef.current;
+		const blueTeamName = blueTeamNameRef.current;
+
+		redTeamName.classList.remove("marquee-left");
+		blueTeamName.classList.remove("marquee-right");
+
+		if (redTeamName.scrollWidth > redTeamName.clientWidth) {
+			redTeamName.classList.add("marquee-left");
+		}
+
+		if (blueTeamName.scrollWidth > blueTeamName.clientWidth) {
+			blueTeamName.classList.add("marquee-right");
+		}
 	};
 
 	useResizeObserver(topbarRef, updateTeamNameBackgroundCutoutWidth);
+	// own resize observer for red and blue team names, with extra functions
+	useResizeObserver(redTeamNameRef, () => {
+		console.log("red team name resized");
+	});
+
+	useEffect(() => {
+		updateTeamNameBackgroundCutoutWidth();
+	}, [redTeamNameRef.current?.clientWidth, blueTeamNameRef.current?.clientWidth]);
 
 	return (
 		<div ref={topbarRef} className="topbar">
 			<div className="red-team-name-wrapper children-center">
-				<div className="red-team-name bold">Red Team</div>
+				<div ref={redTeamNameRef} className="red-team-name bold">
+					Brick4Future ABCDEFGHIJKLMNOPwjfwjfh
+				</div>
 			</div>
 			<div className="countdown-timer-wrapper children-center">
 				<div className="countdown-timer">2:30</div>
 			</div>
 			<div className="blue-team-name-wrapper children-center">
-				<div className="blue-team-name bold marquee">Blue Team</div>
+				<div ref={blueTeamNameRef} className="blue-team-name bold">
+					Mechatronics Hollabrunn
+				</div>
 			</div>
 		</div>
 	);
