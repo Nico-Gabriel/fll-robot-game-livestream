acf70a0d0c5cbfad3cdb957f26f7474af25c871f
Nico-Gabriel Ruckendorfer <nicogabrielruckendorfer@gmail.com>
2025-12-28 00:54:16 +0100

    feat: use the useTimer hook in the timer component to count down from the given duration (in seconds), add additional optional parameters to determine if the timer should have a pre-count (and how long it should be), make the seconds display have a fixed length (2 characters)

--- src/pages/match-stream-page/components/timer/Timer.jsx
+++ src/pages/match-stream-page/components/timer/Timer.jsx
@@ -1,11 +1,19 @@
+import { useTimer } from "react-timer-hook";
 import "./Timer.css";
 
-const Timer = () => {
+const Timer = ({ duration, preCountEnabled = true, preCountDuration = 10 }) => {
+	const calculateExpiryTimestamp = (seconds) => new Date(Date.now() + seconds * 1000);
+
+	const { seconds, minutes } = useTimer({
+		expiryTimestamp: calculateExpiryTimestamp(preCountEnabled ? preCountDuration : duration),
+		autoStart: false,
+	});
+
 	return (
 		<div className="timer">
-			<span className="timer__minutes">2</span>
+			<span className="timer__minutes">{minutes}</span>
 			<span className="timer__colon">:</span>
-			<span className="timer__seconds">30</span>
+			<span className="timer__seconds">{String(seconds).padStart(2, "0")}</span>
 		</div>
 	);
 };
