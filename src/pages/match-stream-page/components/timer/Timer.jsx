3de4b5bc5f98b893f4f84ffedf157ef744808a19
Nico-Gabriel Ruckendorfer <nicogabrielruckendorfer@gmail.com>
2025-12-27 22:56:38 +0100

    feat: create timer component

---
+++ src/pages/match-stream-page/components/timer/Timer.jsx
@@ -0,0 +1,13 @@
+import "./Timer.css";
+
+const Timer = () => {
+	return (
+		<div className="timer">
+			<span className="timer__minutes">2</span>
+			<span className="timer__colon">:</span>
+			<span className="timer__seconds">30</span>
+		</div>
+	);
+};
+
+export default Timer;
