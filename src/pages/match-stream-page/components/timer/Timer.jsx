a78401a08a6b9e3551400e379d554bd24d0aa710
Nico-Gabriel Ruckendorfer <nicogabrielruckendorfer@gmail.com>
2025-12-28 03:55:50 +0100

    feat: implement phase switching in timer (from pre-count to main count), apply css to make the timer red conditionally

--- src/pages/match-stream-page/components/timer/Timer.jsx
+++ src/pages/match-stream-page/components/timer/Timer.jsx
@@ -1,16 +1,40 @@
+import { TimerPhase } from "constants";
+import { useEffect, useState } from "react";
 import { useTimer } from "react-timer-hook";
 import "./Timer.css";
 
 const Timer = ({ duration, preCountEnabled = true, preCountDuration = 10 }) => {
+	const [timerPhase, setTimerPhase] = useState(preCountEnabled ? TimerPhase.PRE : TimerPhase.MAIN);
+
 	const calculateExpiryTimestamp = (seconds) => new Date(Date.now() + seconds * 1000);
 
-	const { seconds, minutes } = useTimer({
+	const onTimerExpire = () => {
+		if (timerPhase !== TimerPhase.PRE) {
+			return;
+		}
+
+		setTimerPhase(TimerPhase.MAIN);
+	};
+
+	const { seconds, minutes, restart } = useTimer({
 		expiryTimestamp: calculateExpiryTimestamp(preCountEnabled ? preCountDuration : duration),
-		autoStart: false,
+		autoStart: true,
+		onExpire: onTimerExpire,
 	});
 
+	useEffect(() => {
+		if (timerPhase !== TimerPhase.MAIN) {
+			return;
+		}
+
+		restart(calculateExpiryTimestamp(duration), true);
+	}, [duration, timerPhase, restart]);
+
+	const isTimerRed =
+		timerPhase === TimerPhase.PRE || (timerPhase === TimerPhase.MAIN && minutes === 0 && seconds <= 10);
+
 	return (
-		<div className="timer">
+		<div className={`timer ${isTimerRed ? "timer--red" : ""}`}>
 			<span className="timer__minutes">{minutes}</span>
 			<span className="timer__colon">:</span>
 			<span className="timer__seconds">{String(seconds).padStart(2, "0")}</span>
