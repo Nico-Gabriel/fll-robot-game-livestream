88276d6a1e3ee7ffb4f37c49835b67e7b17f6382
Nico-Gabriel Ruckendorfer <nicogabrielruckendorfer@gmail.com>
2026-01-03 20:54:57 +0100

    refactor: use import alias instead of relative path when importing sfx from assets

--- src/constants/soundEffectSources.js
+++ src/constants/soundEffectSources.js
@@ -1,8 +1,8 @@
+import beepSound from "assets/audio/sfx/beep.mp3";
+import buzzSound from "assets/audio/sfx/buzz.mp3";
+import highSound from "assets/audio/sfx/high.mp3";
+import lowSound from "assets/audio/sfx/low.mp3";
 import { SoundEffect } from "constants";
-import beepSound from "../assets/audio/sfx/beep.mp3";
-import buzzSound from "../assets/audio/sfx/buzz.mp3";
-import highSound from "../assets/audio/sfx/high.mp3";
-import lowSound from "../assets/audio/sfx/low.mp3";
 
 export const SOUND_EFFECT_SOURCES = Object.freeze({
 	[SoundEffect.PRE_COUNT]: lowSound,
