917d9e02582915e49c5633a197c4f2ec555144e2
Nico-Gabriel Ruckendorfer <nicogabrielruckendorfer@gmail.com>
2026-01-03 20:52:36 +0100

    refactor: convert raw string values of constant 'SoundEffect' to camelCase

--- src/constants/soundEffect.js
+++ src/constants/soundEffect.js
@@ -1,6 +1,6 @@
 export const SoundEffect = Object.freeze({
-	PRE_COUNT: "pre_count",
-	MATCH_START: "match_start",
-	MAIN_COUNT: "main_count",
-	MATCH_END: "match_end",
+	PRE_COUNT: "preCount",
+	MATCH_START: "matchStart",
+	MAIN_COUNT: "mainCount",
+	MATCH_END: "matchEnd",
 });
